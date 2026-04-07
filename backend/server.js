bd01ea59d8a233219e1d27dc7994fee3f4ef1080
Sayan <sayan123@gmail.com>
2026-04-07 13:43:08 +0530

    connected railway db

--- backend/server.js
+++ backend/server.js
@@ -25,14 +25,16 @@ const SALT_ROUNDS = 10;
 let otpStore = {};
 
 const app = express();
+const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection({
-  host:     process.env.DB_HOST || "localhost",
-  user:     process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "",
-  database: process.env.DB_NAME || "users"
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect(err => {
